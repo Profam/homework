b8cc5bc5f845a07d4b30964272b99f8b2deaf0bc
Paul <goldar@tut.by>
2020-02-27 11:25:11 +0300

    change task 11: search SimpleNumber without "break" operator

--- src/by/Rabtsevich/task11/SimpleNumber.java
+++ src/by/Rabtsevich/task11/SimpleNumber.java
@@ -15,19 +15,20 @@ public class SimpleNumber {
     public static void main(String[] args) {
         System.out.print("Введите число: ");
         int x = in.nextInt();
-        boolean isSimple =true;
         if(x<1)
             System.out.println("Ошибка! Введите натуральное число!");
             else if(x % 2 == 0 || x % 5 == 0)
                 System.out.println("число " + x + " не является простым");
                 else {
-                    for (int i = 2; i < Math.sqrt(x); i++) {
-                        if (x / i * i == x) {
-                            isSimple = false;
-                            break;
+                    boolean isSimple = false;
+                    while(!isSimple) {
+                        for (int i = 2; i < Math.sqrt(x); ) {
+                            if (x / i * i == x)
+                                isSimple = true;
+                            else i++;
                         }
                     }
-                    if (isSimple)
+                    if(isSimple = true)
                         System.out.println("простое число");
                     else System.out.println("число " + x + " не является простым");
                 }
