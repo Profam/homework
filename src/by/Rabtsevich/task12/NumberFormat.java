618470d80c7b811b9012b4f55a381bb723147f3f
Paul <goldar@tut.by>
2020-03-02 20:33:39 +0300

    change task 12: added bound checking (0;1000)

--- src/by/Rabtsevich/task12/NumberFormat.java
+++ src/by/Rabtsevich/task12/NumberFormat.java
@@ -15,25 +15,28 @@ public class NumberFormat {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число: ");
         long x = in.nextLong();
-        changeToAccountingFormat(x); //System.out.printf("%,d", x);
+        changeToAccountingFormat(x);  //System.out.printf("%,d", x);
     }
 
    static void changeToAccountingFormat(long x) {
-       String s =  Long.toString(x % 10);
-       x /= 10;
-       int j = 1;
-       for (long i = x; i / 10 != 0; i = x){
-           s += x % 10;
+       if (x / 10 == 0 | x < 1000) System.out.println(x);
+       else {
+           String s = Long.toString(x % 10);
            x /= 10;
-           if(j == 2) {
-               s+=" ";
-               j = 0;
-           } else j++;
+           int j = 1;
+           for (long i = x; i / 10 != 0; i = x) {
+               s += x % 10;
+               x /= 10;
+               if (j == 2) {
+                   s += " ";
+                   j = 0;
+               } else j++;
+           }
+           s += x;
+           for (int i = s.length() - 1; i >= 0; i--) {
+               System.out.print(s.charAt(i));
+           }
        }
-       s+=x;
-       for (int i = s.length()-1; i >= 0; i--) {
-           System.out.print(s.charAt(i));
-       }
-    }
+   }
 }
 
